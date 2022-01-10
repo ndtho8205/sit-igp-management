d99a98fcd4261eb4e74ee270e3eabacfae5bf2e3
Tho Nguyen <ndtho8205@gmail.com>
2022-01-11 03:29:21 +0900

    feat(backend): Add Postgresql connection & Handle CR requests in professors endpoint

--- backend/sit_igp_management_backend/db/base_schema.py
+++ backend/sit_igp_management_backend/db/base_schema.py
@@ -2,10 +2,10 @@ from sqlalchemy import Column, DateTime, func
 from sqlalchemy.orm import declarative_base
 
 
-Base = declarative_base()
+_Base = declarative_base()
 
 
-class BaseSchema(Base):
+class BaseSchema(_Base):
     # pylint: disable=invalid-name
     created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(
